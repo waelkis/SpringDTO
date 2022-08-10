d555af23704ab68dde99c8016707995af09a8383
waelkis <waelkeskes3@gmail.com>
2022-08-10 11:04:44 +0100

    creat Project DTO

---
+++ src/main/java/tn/esps/DtoSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package tn.esps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esps/DtoSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
